a2b404bf728351a3acbc1eb227b54a0eed786ad8
mariomakter <mariom9267idb@gmail.com>
2024-06-16 22:10:47 +0600

    AMS

---
+++ pages/home.php
@@ -0,0 +1,5 @@
+<div class="page-title" style="color:#6295A2; font-style:normal; font-family: Arial, Helvetica, sans-serif; ">Simple Attendance Management System</div>
+<hr>
+<div class="image">
+        <img src="assets/img/img3.jpg" alt="Image not found" style="height:500px;width:100%">
+    </div>
